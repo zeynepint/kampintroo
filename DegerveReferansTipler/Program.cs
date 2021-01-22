30470edbc8cfd9b5bf574413bf2badca6edd6f78
Sonsuz DonguZeynep <pollen.ladinoz@gmail.com>
2021-01-22 17:50:36 +0300

    Add project files.

---
+++ DegerveReferansTipler/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DegerveReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //int sayi1 = 10;
+           // int sayi2 = 30;
+            // sayi1 = sayi2;
+            // sayi2=65
+                //sayi1=30
+
+          
+
+                 
+        }
+    }
+}
---
+++ dongulerr/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace dongulerr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlama Başlangıç İçin Temel Kurs";
+            string kurs3 = "Java";
+            string kurs4 = "Python";
+
+
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı",
+                "Programlamaya Başlangıç İçin Temel Kurs",
+                "Java","Python" };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            Console.WriteLine("sayfa sonu- footer");
+        }
+    }
+}
